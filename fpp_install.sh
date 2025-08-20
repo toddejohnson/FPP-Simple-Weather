cbc0f38f53ef5b729c7ec5153cc75363fd3feb67
Rick Harris <46263877+Poporacer@users.noreply.github.com>
2025-08-20 15:48:13 -0600

    Update fpp_install.sh

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
-/opt/fpp/scripts/update_plugin ${target_PWD##*/}
 echo ; echo “Please restart fppd for new FPP Commands to be visible.” ; echo
 . /opt/fpp/scripts/common
 setSetting restartFlag 1
